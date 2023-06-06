56892146b407a784bb0d9af2e56fe3b90752453c
Arham Khan <arhamkhank353@gmail.com>
2023-06-07 00:49:59 +0500

    ADDED AREAS

---
+++ src/Areas.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './area.css'
+function Areas() {
+  return (
+    <div className='areas'>
+        <h3 className='heading'>Areas Covered Across the city:</h3>
+        <ul>
+        <li>
+            Nishatabad
+        </li>
+        <li>
+            Manawala
+        </li>
+        <li>
+            Khurrianwala
+        </li>
+        <li>
+            Jaranwala
+        </li>
+        <li>
+            Makkuana
+        </li>
+        </ul>
+        <h3>And Many More...</h3>
+    </div>
+  )
+}
+
+export default Areas
